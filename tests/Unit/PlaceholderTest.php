c24910d96727bd5809ac4394a6de8a5146644cf0
fillipeoa <fillipeoa1234@gmail.com>
2026-02-27 19:31:21 -0300

    feat: add placeholder unit test

---
+++ tests/Unit/PlaceholderTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class PlaceholderTest extends TestCase
+{
+    public function test_placeholder(): void
+    {
+        // this test exists so that the Unit testsuite directory is not empty
+        $this->assertTrue(true);
+    }
+}
